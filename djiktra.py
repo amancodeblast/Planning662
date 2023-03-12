a30a95b6ceb3c60df3edab488d221999a07712bc
Aman Sharma <amankrsharma3@gmail.com>
2023-03-12 19:41:44 -0400

    adding the coordinate transform fucntion for opencv conversion

--- djiktra.py
+++ djiktra.py
@@ -1,7 +1,14 @@
 import sys
 import cv2
 
+def org2opencv_conversion(org_coord_start,org_cord_goal, row, col):
+    strt= [org_coord_start[0],row-org_coord_start[1]]
+    goal= [org_cord_goal[0],row-org_cord_goal[1]]
+    print("Start",strt)
+    print("Goal",goal)
 
+
+    
 def main():
     if len(sys.argv) != 5:
         print("Usage: python program_name.py x1 y1 x2 y2")
@@ -16,11 +23,14 @@ def main():
         print("Coordinates must be integers")
         sys.exit(1)
 
-    coord1 = [x1, y1]
-    coord2 = [x2, y2]
+    org_coord_start = [x1, y1]
+    org_cord_goal = [x2, y2]
 
-    print("Start coordiante: {}".format(coord1))
-    print("Goal Coordinate: {}".format(coord2))
+    # print("Start coordiante: {}".format(org_coord_start))
+    # print("Goal Coordinate: {}".format(org_cord_goal))
+    grid_row = 250
+    grid_column = 600
+    org2opencv_conversion(org_coord_start,org_cord_goal, grid_row, grid_column)
 
 if __name__ == "__main__":
     main()
